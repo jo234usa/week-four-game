54ce08c1b2f2419a4bf18410ab5d402b8a3e9f27
Jessica Ong <jessifromblog@gmail.com>
2017-05-07 08:28:18 -0400

    update assets folder

--- assets/javascript/crystal.js
+++ assets/javascript/crystal.js
@@ -4,7 +4,7 @@ var losses = 0;
 
 var RandomNumber = generateRandomNum(19,120);
 
-var gemsArray = [];
+var gemsArray = $(".gems");
 
 var userScore = 0;
 
@@ -15,36 +15,60 @@ function generateRandomNum(min, max) {
 	return Math.floor(Math.random () * (max - min+1)) + min;
 }
 
-function gameDisplays() {
-	// $("#winsDisplay").append(wins);
-	// $("#lossesDisplay").append(losses);
-	$("#displayRandomNumber").append(RandomNumber);
-	// $("#displaysYourTotalScore").append(userScore);
-}
-
 //pseudo code step 2: assign random number to gems btw. 1-12
 function gemNum(){
 	for (var i=0; i < gemsArray.length; i++) {
-		$("img").each(function(){
-			var id = $(this).attr('id');
-    		$(gemsArray).push(id);
-			generateRandomNum(1, 12);
-			console.log(diffGems);
-		});
-		// gemsArray[i].value = generateRandomNum(1, 12);
+		gemsArray[i].value = generateRandomNum(1, 12);
 	}
 }
 
-gemNum()
+function intGame() {
+	$("#winsDisplay").append(wins);
+	$("#lossesDisplay").append(losses);
+	$("#displayRandomNumber").append(RandomNumber);
+	$("#displaysYourTotalScore").append(userScore);
+	gemNum();
+}
 
-gameDisplays()
-//pseudo code step 3: when player clicks on gems add randomly generated number to scoreboard
-// $(gemsArray).on("click", function(){
-// 	var gemVal = this.value;
-// 	userScore = userScore + gemVal;
-// })
 //pseudo code step 4: if score = random no. increase win, else if score > random number increase loss
+function winOrLose() {
+	if (RandomNumber === userscore){
+		wins++;
+		$("#winsDisplay").append(wins);
+		reset()
+	}
+	else if (RandomNumber < userscore){
+		losses++;
+		$("#lossesDisplay").append(losses);
+		reset()
+	}
+}
+
 //pseudo code step 5: reset userscore & gem numbers & regenerate a new random number to the screen
+function reset(){
+	userscore = 0;
+	RandomNumber = generateRandomNum(19,120);
+	gemNum();
+	$("#winsDisplay").append(wins);
+	$("#lossesDisplay").append(losses);
+	$("#displayRandomNumber").append(RandomNumber);
+	$("#displaysYourTotalScore").append(userScore);
+}
+
+//-----------MAIN PROGRAM---------------------------------
+intGame()
+//pseudo code step 3: when player clicks on gems add randomly generated number to scoreboard
+$(gemsArray).on("click", function(event){
+	console.log(event);
+	console.log(this);
+	console.log(this.value);
+	var gemVal = this.value;
+	userScore = userScore + gemVal;
+	console.log(userscore);
+	$("#displaysYourTotalScore").html(userscore)
+	winOrLose();
+});
+
 
 
 
